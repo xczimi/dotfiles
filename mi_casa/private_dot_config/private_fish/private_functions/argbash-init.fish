c9f7dc711bd380cbc4d44d921dccf0f9e062cf28
Peter Czimmermann <peter.czimmermann@sensorup.com>
2022-03-30 18:01:50 -0700

    feat: remove argbash

--- mi_casa/private_dot_config/private_fish/private_functions/argbash-init.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function argbash-init --description 'alias argbash-init docker run -ti --rm -e PROGRAM=argbash-init -v {$PWD}:/work matejak/argbash'
-	docker run -ti --rm -e PROGRAM=argbash-init -v {$PWD}:/work matejak/argbash $argv;
-end
--- mi_casa/private_dot_config/private_fish/private_functions/argbash.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function argbash --description 'alias argbash docker run -ti --rm -e PROGRAM=argbash -v {$PWD}:/work matejak/argbash'
-	docker run -ti --rm -e PROGRAM=argbash -v {$PWD}:/work matejak/argbash $argv;
-end
